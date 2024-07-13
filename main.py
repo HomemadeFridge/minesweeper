66b1f64f119ae0e14508fe33f9d15e755533d743
HomemadeFridge <121769446+HomemadeFridge@users.noreply.github.com>
2024-07-13 21:11:50 +0100

    Added customisable board before pygame window opens. The board renders with text above saying minesweeper! Needs some changes (swapping bg colours)

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ pygame.init()
 #PYGAME WINDOW SETUP
 display = pygame.display.set_mode((1000,600))
 pygame.display.set_icon(pygame.image.load('resources/gameicon.png'))
+framerate = pygame.time.Clock()
 
 #VARIABLES
 carryOn = True
+rows = int(input('How many rows?\n>')); rowdivison = 1000//rows
+columns = int(input('How many columns?\n>')); columndivision = 500//columns
 
 #COLOURS
 BLACK = (0,0,0)
@@ -17,16 +20,30 @@ BGCOLOUR = [(192,192,192),(128,128,128)]
 font = pygame.font.Font("mine-sweeper.TTF",20)
 
 #GRID
-grid = [[0 for i in range(0,9)] for i in range(0,9)]
-print(grid)
+grid = [[0 for i in range(0,rows)] for i in range(0,columns)]
 
 #FUNCTIONS
 
-
+def drawGrid(pixels):
+    global display, rows, columns, BGCOLOUR
+    for i in range(0,columns):
+        for j in range(0,rows):
+            pygame.draw.rect(display,(128,128,128),[(j*pixels)+(500-((rows/2)*pixels)),(i*pixels)+100,pixels,pixels])
+            pygame.draw.rect(display,(192,192,192),[(j*pixels)+(500-((rows/2)*pixels)),(i*pixels)+100,pixels,pixels],2)
 
 #MAIN LOOP
 while carryOn:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             carryOn = False
-    display.fill(BLACK)
+    display.fill((192,192,192))
+
+    if columndivision < rowdivison:
+        drawGrid(columndivision)
+    else:
+        drawGrid(rowdivison)
+        
+    pygame.draw.rect(display,(128,128,128),[0,0,1000,100])
+
+    pygame.display.flip()
+    framerate.tick(60)
